af289475067f32d35f413df64725c4df15d03391
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-08-26 18:21:12 -0700

    DNS provider

--- utilities/dns/fb-dns.tf
+++ utilities/dns/fb-dns.tf
@@ -1,3 +1,12 @@
 variable "fb_key_name" {}
 variable "fb_key_algorithm" {}
 variable "fb_key_secret" {}
+
+provider "dns" {
+    update {
+        server = "72.167.238.111"
+        key_name = "${var.fb_key_name}"
+        key_algorithm = "${var.fb_key_algorithm}"
+        key_secret = "${var.fb_key_secret}"
+    }
+}
